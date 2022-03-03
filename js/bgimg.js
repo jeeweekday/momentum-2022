a144f45417dd019db68d416c09d27dad28d1c9e8
jeeweekday <whops92@gmail.com>
2022-03-03 21:03:29 +0900

    Update background img JS and add reset CSS

---
+++ js/bgimg.js
@@ -0,0 +1,8 @@
+const imgs = ["0.JPG","1.JPG","2.JPG"];
+const pickImg = imgs[Math.floor(Math.random() * imgs.length)];
+// 중간에 pickNum을 만들려 했지만 이미 quotes.js에서 선언을해서 따로 변수 선언을 안하고 그냥 랜덤숫자뽑음.
+const bgImg = document.createElement("img");
+bgImg.src = `img/${pickImg}`;
+bgImg.classList.add("bg-img");
+document.body.appendChild(bgImg);
+// prependChild or insertBefore을 사용해서도 삽입가능.
