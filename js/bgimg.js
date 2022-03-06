64b80b0004c014ab983f8a71c36899215f458d25
jeeweekday <whops92@gmail.com>
2022-03-06 18:48:18 +0900

    Edit CSS

--- js/bgimg.js
+++ js/bgimg.js
@@ -1,8 +1,9 @@
 const imgs = ["0.JPG","1.JPG","2.JPG"];
 const pickImg = imgs[Math.floor(Math.random() * imgs.length)];
-// 중간에 pickNum을 만들려 했지만 이미 quotes.js에서 선언을해서 따로 변수 선언을 안하고 그냥 랜덤숫자뽑음.
+// 중간에 변수 pickNum을 만들려 했지만 이미 quotes.js에서 선언을해서 따로 변수 선언을 안하고 그냥 랜덤숫자뽑음.
+const bgBox = document.querySelector("#bg-box");
 const bgImg = document.createElement("img");
 bgImg.src = `img/${pickImg}`;
 bgImg.classList.add("bg-img");
-document.body.appendChild(bgImg);
+bgBox.appendChild(bgImg);
 // prependChild or insertBefore을 사용해서도 삽입가능.
